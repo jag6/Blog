6c70aa3885cc61c3074ee3f5cfb94033ba1056f6
Matt <matt@randomforestweb.com>
2022-08-17 20:42:57 +0900

    day 2

--- backend/routers/articleRouter.js
+++ backend/routers/articleRouter.js
@@ -1,9 +1,50 @@
 import express from "express";
+import Article from "../models/articleModel";
 
 const articleRouter = express.Router();
-articleRouter.get('/', (req, res) => {
-    res.send('article');
+
+//creates new article page
+articleRouter.get('/new', (req, res) => {
+    res.render('articles/new', { article: new Article() });
 });
 
+//shows articles
+articleRouter.get('/:slug', async (req, res) => {
+    const article = await Article.findOne({ slug: req.params.slug});
+    if(article == null) res.redirect('/')
+    res.render('articles/show', { article: article })
+});
+
+//creates new article
+articleRouter.post('/', async (req, res) => {
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    });
+    try {
+        article = await article.save();
+        res.redirect(`/articles/${article.slug}`)
+    }catch (e) {
+        console.log(e);
+        res.render('articles/new', { article: article })
+    }
+});
+
+//deletes article
+articleRouter.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+});
+
+/*articleRouter.delete(':/id', async (req, res) => {
+    const article = await Article.findById(req.params.id);
+    if(article) {
+        const deletedArticle = await article.remove();
+        res.send ({message: 'Article Deleted,', article: deletedArticle})
+    }else {
+        res.status(404).send({message: 'Article Not Found'})
+    }
+})*/
 
 export default articleRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,36 @@
 import express from "express";
+import mongoose from "mongoose";
 import articleRouter from "./routers/articleRouter";
+import Article from "./models/articleModel";
+import methodOverride from "method-override";
 
 const app = express();
+mongoose.connect('mongodb://localhost/blog', 
+    {   useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('connected to mongodb');
+    }).catch((error) => {
+        console.log(error.reason);
+    });
 
-app.set("view engine", 'ejs')
+app.set("view engine", 'ejs');
+
+app.use(express.static("css")); 
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride('_method'));
 
 app.use('/articles', articleRouter);
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test',
-        createdAt: new Date(),
-        description: 'test'
-    }]
-    res.render('index', { articles: articles})
-})
-
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort(
+        { createdAt: 'descending'});
+    res.render('articles/index', { articles: articles})
+});
 
 const port = 5000;
-
 app.listen(port, () => {
     console.log(`serving at localhost:${port}`)
 });
