edf5926f2c093bf5eb5126347e5c08146f36b332
Matt <matt@randomforestweb.com>
2022-08-30 01:37:35 +0900

    top-btn again

--- public/scripts/top-btn.js
+++ public/scripts/top-btn.js
@@ -1,19 +1,18 @@
 const topBtn = document.getElementById("topBtn");
 
-window.addEventListener('scroll', () => {
-  const scrollFunction = () => {
+const scrollFunction = () => {
+  window.addEventListener('scroll', () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       topBtn.style.display = "flex";
     } else {
       topBtn.style.display = "none";
     }
-  }
-  scrollFunction();
-});
+  });
+}
 
 const topFunction = () => {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
 
-topFunction();
+scrollFunction(), topFunction();
